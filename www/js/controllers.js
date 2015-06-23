956ffc7e078177d8f4afa1e0fd921c0383a242ab
Swaminathan Vetri <svswaminathan@gmail.com>
2015-06-23 09:04:12 +0530

    Initial Commit
    Initial Commit of the sample code base

---
+++ www/js/controllers.js
@@ -0,0 +1,43 @@
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ListCtrl', function($scope) {
+  $scope.viewModel = {};
+  $scope.viewModel.showDelete = false;
+  $scope.viewModel.showReorder = false;
+  
+  $scope.showDeleteButton = function(){
+    $scope.viewModel.showDelete = !$scope.viewModel.showDelete;
+  }
+  
+  $scope.showReorderButton = function(){
+    $scope.viewModel.showReorder = !$scope.viewModel.showReorder; 
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
